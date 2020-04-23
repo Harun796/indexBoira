a5c11d7efeadcb3c926b34e4d64e6fc1acc23406
Harun796 <harun.meraga@iesjoandaustria.org>
2020-04-23 09:33:53 +0000

    exercici 4 acabat correctament

--- boira.py
+++ boira.py
@@ -26,15 +26,15 @@ def mitjanaParaulesPerFrase(contingut):
 def numeroParulesComplexes(contingut):
   #TODO Heu de retornar el nombre de paraules complexes que té el text
   complexos = 0
-  for z in text:
+  for z in contingut:
     if(len(z)>5):
       complexos = complexos + 1 
 
-  #Són aquelles que tenen méés de cinc lletres
   return complexos
 
+
 def percetantgeParaulesComplexes(contingut):
-  percentatge =100*((numeroParulesComplexes(contingut))/(comptarParaules(contingut))
+  percentatge = 100*((numeroParulesComplexes(contingut))/(comptarParaules(contingut)))
   return percentatge
 
 #TODO Fer-ho al final de tot el 5.
@@ -59,12 +59,20 @@ contingut= text.read()
 
 #Exercici 4
 #càlcul de la fórmula
-print(comptarParaules(contingut))
-print(comptarFrases(contingut))
-IndexBoira = 0.4 * (mitjanaParaulesPerFrase(contingut))(percetantgeParaulesComplexes(contingut))
+IndexBoira = 0.4 * (mitjanaParaulesPerFrase(contingut))-(percetantgeParaulesComplexes(contingut))
 
 #TODO  Pinteu index per pantalla 
 print(IndexBoira)
+if(round(IndexBoira) == 6):
+  print("Sixth grade")
+elif(round(IndexBoira) == 7):
+  print("Seventh grade")
+elif(round(IndexBoira) == 8):
+  print("Eighth grade")
+elif(round(IndexBoira) == 9 or round(IndexBoira) == 10 or round(IndexBoira) == 11 or round(IndexBoira) == 12):
+  print("High school")
+elif(round(IndexBoira) == 13 or round(IndexBoira) == 14 or round(IndexBoira) == 15 or round(IndexBoira) == 16 or round(IndexBoira) == 17):
+  print("College")
 #TODO Classifiqueu exercici segons índex
 # Digueu com és el text seguint la taula https://en.wikipedia.org/wiki/Gunning_fog_inde
 #TODO
